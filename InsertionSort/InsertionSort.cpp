b2a38a5da6e9ebe406bb090a6ae082abf92ea9e6
adwitafernanda <adwitafernanda4@gmail.com>
2023-03-23 10:23:36 +0700

    Add project files.

---
+++ InsertionSort/InsertionSort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data pada Array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{ }
+	}
+}
