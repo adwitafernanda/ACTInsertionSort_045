1826841a5ec8f0638402f62a09543a2a33333588
adwitafernanda <adwitafernanda4@gmail.com>
2023-03-23 10:35:53 +0700

    InsertionSort

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -15,7 +15,13 @@ void input() {
 		}
 		else
 		{
-			cout << "\nArray"
+			cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";
 		}
 	}
+	cout << endl;
+	cout << "=====================" << endl;
+	cout << "Masukkan Elemen Array" << endl;
+	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++)
 }
