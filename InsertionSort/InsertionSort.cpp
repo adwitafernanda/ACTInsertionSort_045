ca9bcba8a6a4447596ba2809d40ea8b42a70c1b4
adwitafernanda <adwitafernanda4@gmail.com>
2023-03-23 11:27:51 +0700

    InsertionSort

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -57,3 +57,22 @@ void insertionsort() {
 
 	}
 }
+
+void display() {
+	cout << endl;
+	cout << "\=================================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "=================================" << endl;
+
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	input();
+	insertionsort();
+	display();
+}
